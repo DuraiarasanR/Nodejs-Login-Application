8599d00974f0bcb9ccd9f4f9dadaa2e3c25dbda4
Duraiarasan <duraiarasan1808@gmail.com>
2021-05-03 20:03:24 +0530

    Our first commit

---
+++ controllers/userController.js
@@ -0,0 +1,56 @@
+const User = require("../models/User");
+
+exports.login = function (req, res) {
+  let user = new User(req.body);
+  user
+    .login()
+    .then(function (result) {
+      req.session.user = { favColor: "blue", username: user.data.username };
+      req.session.save(function () {
+        res.redirect("/");
+      });
+    })
+    .catch(function (e) {
+      req.flash("errors", e);
+      req.session.save(function () {
+        res.redirect("/");
+      });
+    });
+};
+
+exports.logout = function (req, res) {
+  req.session.destroy(function () {
+    res.redirect("/");
+  });
+};
+
+exports.register = function (req, res) {
+  let user = new User(req.body);
+  user
+    .register()
+    .then(() => {
+      req.session.user = { username: user.data.username };
+      req.session.save(function () {
+        res.redirect("/");
+      });
+    })
+    .catch((regErrors) => {
+      regErrors.forEach(function (error) {
+        req.flash("regErrors", error);
+      });
+      req.session.save(function () {
+        res.redirect("/");
+      });
+    });
+};
+
+exports.home = function (req, res) {
+  if (req.session.user) {
+    res.render("home-dashboard", { username: req.session.user.username });
+  } else {
+    res.render("home-guest", {
+      errors: req.flash("errors"),
+      regErrors: req.flash("regErrors"),
+    });
+  }
+};
