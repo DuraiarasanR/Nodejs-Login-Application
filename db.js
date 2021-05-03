6147c27a3aa438dd4f1b27f0c39ddb725b9e02ae
Duraiarasan <duraiarasan1808@gmail.com>
2021-05-03 21:15:49 +0530

    First commit

--- db.js
+++ db.js
@@ -1,12 +1,7 @@
 const dotenv = require("dotenv");
 dotenv.config();
-
 const mongodb = require("mongodb");
-const { default: validator } = require("validator");
-let port = process.env.PORT;
-if (port == null || port == "") {
-  port = 3000;
-}
+
 mongodb.connect(
   process.env.CONNECTIONSTRING,
   { useNewUrlParser: true, useUnifiedTopology: true },
